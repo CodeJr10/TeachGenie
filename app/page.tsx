1b5c84e41a2115a6d6326b7f3365dc41291d5dd8
CodeJr10 <aadityamalap666@gmail.com>
2025-05-28 18:32:29 +0530

    Initialized Routing system

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold text-center mb-4">Page</h1>
+      <Button>Start</Button>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
