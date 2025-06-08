f23bfd3f145a9c4a715ba0a343603d8f35362ab7
CodeJr10 <aadityamalap666@gmail.com>
2025-06-08 22:35:24 +0530

    chore: implement searchParams for companions library

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,4 +1,14 @@
-const CompanionsLibrary = () => {
+import { getAllCompanions } from "@/lib/actions/companions.action";
+
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
+
+  const companions = await getAllCompanions({ subject, topic });
+
+  console.log("Companions:", companions);
+
   return <div>Companions</div>;
 };
 
