5d8558e2975d8f34935ced4b9835162c03c55108
CodeJr10 <aadityamalap666@gmail.com>
2025-06-11 21:41:19 +0530

    feat: Add companion session page props

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,4 +1,18 @@
-const CompanionSession = () => {
+import { currentUser } from "@clerk/nextjs/server";
+import { getCompanion } from "@/lib/actions/companions.action";
+import { redirect } from "next/navigation";
+
+interface CompanionSessionPageProps {
+  params: Promise<{ id: string }>;
+}
+const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
+  const { id } = await params;
+  const companion = await getCompanion(id);
+  const user = await currentUser();
+
+  if (!user) redirect("/sign-in");
+  if (!companion) redirect("/companions");
+
   return <div>CompanionSession</div>;
 };
 
